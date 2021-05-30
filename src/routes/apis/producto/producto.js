c2ed5464ed7a45691614b94c23c6e9e87d25f20e
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-05-30 12:38:46 -0500

    se ajusta ruta de crear productos que no tenia el res.json

--- src/routes/apis/producto/producto.js
+++ src/routes/apis/producto/producto.js
@@ -2,26 +2,28 @@ const express = require('express');
 const router = express.Router();
 const productoController = require('../../../controllers/producto/producto');
 
-router.get('/listar', async(req, res) =>{
+router.get('/listar', async (req, res) => {
     const productos = await productoController.Productos(req.body);
     res.json({ productos });
 });
 
-router.post('/listar_producto/:pro_codigo', async(req, res) =>{
+router.post('/listar_producto/:pro_codigo', async (req, res) => {
 
-    const  pro_codigo = req.params.pro_codigo;
+    const pro_codigo = req.params.pro_codigo;
 
     const list_producto = await productoController.ListProducto(pro_codigo);
     res.json({ list_producto });
 });
 
-router.post('/crear', async(req, res) =>{
+router.post('/crear', async (req, res) => {
     const crear_producto = await productoController.RegistroProducto(req.body);
+    res.json({ msg: "producto creado exitosamente" });
+
 });
 
-router.put('/modificar/:pro_codigo', async(req, res) =>{
-    
-    const  pro_codigo = req.params.pro_codigo;
+router.put('/modificar/:pro_codigo', async (req, res) => {
+
+    const pro_codigo = req.params.pro_codigo;
 
     const modificar_producto = await productoController.ModidicarProducto(pro_codigo, req.body);
 });
