e54e169c67ccf2c8f4198cf592e6207391ccc81b
Erick Andres Vergara Noriega <erickandres30@gmail.com>
2021-05-30 18:07:55 -0500

    ajuste de model y rutas de personas

--- src/models/persona/persona.js
+++ src/models/persona/persona.js
@@ -1,40 +1,40 @@
 const pool = require('../../database');
 
 module.exports = function () {
-   
-    async function Personas(req){
-        
+
+    async function Personas(req) {
+
         const data = await pool.query('SELECT per_codigo, per_nombre, per_apellido, per_correo, per_password, per_perfil, perf_nombre FROM persona, perfil WHERE per_perfil = perf_codigo');
-        
+
         return data
     }
 
-    async function ListPersona(per_codigo){
-        
-        const data = await pool.query('SELECT per_codigo, per_nombre, per_apellido, per_correo, per_password, per_perfil, perf_nombre FROM persona, perfil WHERE per_perfil = perf_codigo AND per_codigo = ?', [per_codigo]);
-        
+    async function ListPersona(per_codigo) {
+
+        const data = await pool.query('SELECT per_codigo, per_nombre, per_apellido, per_correo, per_password, per_perfil FROM persona WHERE  per_codigo = ?', [per_codigo]);
+
         return data
     }
 
-    async function RegistroPersona(req){
+    async function RegistroPersona(req) {
 
-        const data = await pool.query('INSERT INTO persona set ?',[req]);
-
-    }
-    
-
-    async function ModificarPersona(per_codigo, req){
-
-        const data = await pool.query('UPDATE persona SET  ? WHERE per_codigo = ?',[req, per_codigo]);
+        const data = await pool.query('INSERT INTO persona set ?', [req]);
 
     }
 
 
+    async function ModificarPersona(per_codigo, req) {
 
-    return{
+        const data = await pool.query('UPDATE persona SET  ? WHERE per_codigo = ?', [req, per_codigo]);
+
+    }
+
+
+
+    return {
         Personas,
         RegistroPersona,
-        ModificarPersona, 
+        ModificarPersona,
         ListPersona,
     }
 }
--- src/routes/apis/persona/persona.js
+++ src/routes/apis/persona/persona.js
@@ -2,26 +2,26 @@ const express = require('express');
 const router = express.Router();
 const personaController = require('../../../controllers/persona/persona');
 
-router.get('/listar', async(req, res) =>{
+router.get('/listar', async (req, res) => {
     const personas = await personaController.Personas(req.body);
     res.json({ personas });
 });
 
-router.post('/listar_persona/:per_codigo', async(req, res) =>{
+router.get('/listar_persona/:per_codigo', async (req, res) => {
 
-    const  per_codigo = req.params.per_codigo;
+    const per_codigo = req.params.per_codigo;
 
     const list_persona = await personaController.ListPersona(per_codigo);
     res.json({ list_persona });
 });
 
-router.post('/crear', async(req, res) =>{
+router.post('/crear', async (req, res) => {
     const crear_persona = await personaController.RegistroPersona(req.body);
 });
 
-router.put('/modificar/:per_codigo', async(req, res) =>{
-    
-    const  per_codigo = req.params.per_codigo;
+router.put('/modificar/:per_codigo', async (req, res) => {
+
+    const per_codigo = req.params.per_codigo;
 
     const modificar_producto = await personaController.ModificarPersona(per_codigo, req.body);
 });
